1a1e7269f71754abde6810a78e1560c2fcaba263
chanyeong|YU <uchan01001@gmail.com>
2020-08-12 17:28:37 +0900

    news url 연결

--- django_src/blog/models.py
+++ django_src/blog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
-# Create your models here.
 class News(models.Model):
+    objects = None
     news_title = models.CharField(db_column="제목", max_length=400)
     news_url = models.URLField(db_column="링크", max_length=400)
     image_link = models.CharField(db_column="이미지", max_length=400)
-    published_date = models.DateTimeField(db_column="등록날짜", blank=True, null=True)
+    published_date = models.CharField(db_column="등록날짜", max_length=400)
 
     class Meta:
-        managed = True
-        db_table = "article_cloud"
+        managed = False
+        db_table = "article_new_tech"
+
+    def __str__(self):
+        return self.news_title
