b4f1ab7ad47283882f8099d4ae6cd6c7ab764e76
Minji <kmj1995kr@gmail.com>
2020-08-12 18:05:00 +0900

    jobs model added

--- django_src/blog/models.py
+++ django_src/blog/models.py
@@ -13,3 +13,21 @@ class News(models.Model):
 
     def __str__(self):
         return self.news_title
+
+
+class Jobs(models.Model):
+    company = models.CharField(db_column='기업명', max_length=100)
+    job_title = models.CharField(db_column='직무', max_length=300)
+    experience = models.CharField(db_column='경력사항', max_length=300)
+    edu_level = models.CharField(db_column='학력', max_length=50)
+    type = models.CharField(db_column='근무형태', max_length=50)
+    location = models.CharField(db_column='근무지', max_length=50)
+    category = models.CharField(db_column='분류', max_length=300)
+    link = models.CharField(db_column='상세링크', max_length=300)
+
+    class Meta:
+        managed = False
+        db_table = "jobs_cloud"
+
+    def __str__(self):
+        return self.job_title
