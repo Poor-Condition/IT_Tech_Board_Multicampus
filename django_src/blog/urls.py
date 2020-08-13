706a6e4364abb8418425f2822d2c3352b926718b
kmj1995kr <32619843+kmj1995kr@users.noreply.github.com>
2020-08-13 21:20:01 +0900

    Revert "Chan"

--- django_src/blog/urls.py
+++ django_src/blog/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.main_view, name='main_view'),
-    path('article/', views.article_new_tech_list, name='article_new_tech_list'),
+    path('', views.article_new_tech_list, name='article_new_tech_list'),
 ]
