618e192dea3e9c7ea9b8c73ad4bf2e12442c9988
chanyeong|YU <uchan01001@gmail.com>
2020-08-14 09:34:13 +0900

    article

--- django_src/blog/views.py
+++ django_src/blog/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render,  get_object_or_404
 
-from .models import News
+from .models import News_dev, News_cloud, News_new_tech
 
 def main_view(request):
     return render(request, "blog/main_view.html",)
 
-def article_new_tech_list(request):
-    articles = News.objects.all()
+def article_list(request):
+    return render(request, "blog/article/article_list.html",)
 
-    return render(request, "blog/article_new_tech_list.html", {"articles": articles})
+def article_dev_list(request):
+    article_dev = News_dev.objects.all()
+
+    return render(request, "blog/article/article_detail_list.html", {"articles": article_dev})
+
+def article_cloud_list(request):
+    article_cloud = News_cloud.objects.all()
+
+    return render(request, "blog/article/article_detail_list.html", {"articles": article_cloud})
+
+def article_new_tech_list(request):
+    article_new_tech = News_new_tech.objects.all()
+
+    return render(request, "blog/article/article_detail_list.html", {"articles": article_new_tech})
