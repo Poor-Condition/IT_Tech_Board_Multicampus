8ade428ab45e6a740b94487c06f30efeb9521784
Minji <kmj1995kr@gmail.com>
2020-08-19 16:37:01 +0900

    jobs_cloud, jobs_db, jobs_python deleted

--- django_src/blog/models.py
+++ django_src/blog/models.py
@@ -80,63 +80,6 @@ class Jobs(models.Model):
     def __str__(self):
         return self.job_title
 
-class Jobs_Cloud(models.Model):
-    objects = None
-
-    company = models.CharField(db_column='기업명', max_length=100)
-    job_title = models.CharField(db_column='직무', max_length=300)
-    experience = models.CharField(db_column='경력사항', max_length=300)
-    edu_level = models.CharField(db_column='학력', max_length=50)
-    type = models.CharField(db_column='근무형태', max_length=50)
-    location = models.CharField(db_column='근무지', max_length=50)
-    category = models.CharField(db_column='분류', max_length=300)
-    link = models.CharField(db_column='상세링크', max_length=300)
-
-    class Meta:
-        managed = False
-        db_table = "job_list"
-
-    def __str__(self):
-        return self.job_title
-
-class Jobs_Python(models.Model):
-    objects = None
-
-    company = models.CharField(db_column='기업명', max_length=100)
-    job_title = models.CharField(db_column='직무', max_length=300)
-    experience = models.CharField(db_column='경력사항', max_length=300)
-    edu_level = models.CharField(db_column='학력', max_length=50)
-    type = models.CharField(db_column='근무형태', max_length=50)
-    location = models.CharField(db_column='근무지', max_length=50)
-    category = models.CharField(db_column='분류', max_length=300)
-    link = models.CharField(db_column='상세링크', max_length=300)
-
-    class Meta:
-        managed = False
-        db_table = "job_list"
-
-    def __str__(self):
-        return self.job_title
-
-class Jobs_DB(models.Model):
-    objects = None
-
-    company = models.CharField(db_column='기업명', max_length=100)
-    job_title = models.CharField(db_column='직무', max_length=300)
-    experience = models.CharField(db_column='경력사항', max_length=300)
-    edu_level = models.CharField(db_column='학력', max_length=50)
-    type = models.CharField(db_column='근무형태', max_length=50)
-    location = models.CharField(db_column='근무지', max_length=50)
-    category = models.CharField(db_column='분류', max_length=300)
-    link = models.CharField(db_column='상세링크', max_length=300)
-
-    class Meta:
-        managed = False
-        db_table = "job_list"
-
-    def __str__(self):
-        return self.job_title
-
 
 class Contest_game(models.Model):
     objects=None
